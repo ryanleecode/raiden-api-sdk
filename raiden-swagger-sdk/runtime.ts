dc68438468fd0d8d7b718039406cae5151998c42
Ryan Lee <drdgvhbh@gmail.com>
2019-07-31 00:52:06 -0400

    Update base path in swagger sdk

--- raiden-swagger-sdk/runtime.ts
+++ raiden-swagger-sdk/runtime.ts
@@ -15,7 +15,7 @@ import { Observable, of } from 'rxjs';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { map, concatMap } from 'rxjs/operators';
 
-export const BASE_PATH = 'http://localhost'.replace(/\/+$/, '');
+export const BASE_PATH = 'http://127.0.0.1:5001/api/v1'.replace(/\/+$/, '');
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
