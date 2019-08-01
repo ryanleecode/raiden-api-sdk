178f875a524c135d1b1e12d4acb61b3685796c75
Ryan <drdgvhbh@gmail.com>
2019-07-31 23:49:21 -0400

    Add JsDoc link for address

--- raiden-node-sdk/src/node.ts
+++ raiden-node-sdk/src/node.ts
@@ -13,6 +13,7 @@ export class Node {
    * @summary Query your address
    * @description When raiden starts, you choose an ethereum address which will
    * also be your raiden address.
+   * @link https://raiden-network.readthedocs.io/en/stable/rest_api.html#get--api-(version)-address
    */
   public address(): Observable<Address> {
     return this.nodeApi.getAddress();
