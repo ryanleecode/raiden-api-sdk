b9f531388b63a0428ff2199cd8eaf039d39e1fb6
Ryan Lee <drdgvhbh@gmail.com>
2019-08-02 19:35:51 -0400

    Complete SDK

--- raiden-node-sdk/src/raiden.ts
+++ raiden-node-sdk/src/raiden.ts
@@ -3,12 +3,16 @@ import { Node } from "./node";
 import { Channels } from "./channels";
 import { Tokens } from "./tokens";
 import { PendingTransfers } from "./pendingTransfers";
+import { TokenNetworks } from "./token-networks";
+import { Payments } from "./payments";
 
 export interface RaidenSwaggerApis {
   node: Node;
   channels: Channels;
   tokens: Tokens;
   pendingTransfers: PendingTransfers;
+  tokenNetworks: TokenNetworks;
+  payments: Payments;
 }
 
 export class Raiden {
@@ -23,23 +27,41 @@ export class Raiden {
 
   public readonly pendingTransfers: PendingTransfers;
 
+  public readonly tokenNetworks: TokenNetworks;
+
+  public readonly payments: Payments;
+
   public static create(config?: Configuration): Raiden {
     const node = Node.create(config);
     const channels = Channels.create(config);
     const tokens = Tokens.create(config);
     const pendingTransfers = PendingTransfers.create(config);
-    return new Raiden({ node, channels, tokens, pendingTransfers });
+    const tokenNetworks = TokenNetworks.create(config);
+    const payments = Payments.create(config);
+
+    return new Raiden({
+      node,
+      channels,
+      tokens,
+      pendingTransfers,
+      tokenNetworks,
+      payments
+    });
   }
 
   public constructor({
     node,
     channels,
     tokens,
-    pendingTransfers
+    pendingTransfers,
+    tokenNetworks,
+    payments
   }: RaidenSwaggerApis) {
     this.node = node;
     this.channels = channels;
     this.tokens = tokens;
     this.pendingTransfers = pendingTransfers;
+    this.tokenNetworks = tokenNetworks;
+    this.payments = payments;
   }
 }
