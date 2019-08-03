539c83ef6ad365fe9afe6eb0c5f2da0b7f4d94e5
Ryan Lee <drdgvhbh@gmail.com>
2019-08-03 11:35:53 -0400

    Remove "Official Documentation" pipe

--- src/node.ts
+++ src/node.ts
@@ -32,7 +32,7 @@ export class Node {
    * })()
    * ```
    *  <br />
-   * {@link https://raiden-network.readthedocs.io/en/stable/rest_api.html#get--api-(version)-address | Official API Documentation}
+   * {@link https://raiden-network.readthedocs.io/en/stable/rest_api.html#get--api-(version)-address}
    */
   public ourAddress(): Observable<string> {
     return this.nodeApi.getAddress().pipe(
