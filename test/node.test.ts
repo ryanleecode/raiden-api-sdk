00c9ccd850376c787b52840ff98ee0376ca0beae
Ryan <drdgvhbh@gmail.com>
2019-08-04 13:47:36 -0400

    Make return value only being mocked once

--- test/node.test.ts
+++ test/node.test.ts
@@ -10,7 +10,7 @@ describe('raiden node', () => {
   it('should return our address as a string', async (done) => {
     const ourAddress = '0xeD7f5B7249b88A17425478D2c114972EA960Fd2A';
     const ajaxMock = (ajax as unknown) as jest.Mock<AjaxCreationMethod>;
-    ajaxMock.mockReturnValue(of({
+    ajaxMock.mockReturnValueOnce(of({
       response: { our_address: ourAddress },
       status: Http.OK,
     }) as any);
