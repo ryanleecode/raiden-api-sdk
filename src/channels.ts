a830fdea3464630d569ad981a908c705fecc076d
Ryan <drdgvhbh@gmail.com>
2019-08-04 20:16:12 -0400

    Add code example for find all unsettled channels for token addr

--- src/channels.ts
+++ src/channels.ts
@@ -76,6 +76,33 @@ export class Channels {
   /**
    * List of all unsettled channels for the given token address
    *
+   * @example
+   * ```typescript
+   * import Raiden from 'raiden-sdk';
+   *
+   * (async function() {
+   *   const raiden = Raiden.create();
+   *   const tokenAddress = '0xaFF4481D10270F50f203E0763e2597776068CBc5';
+   *   const unsettledChannels = await raiden.channels
+   *     .findAllUnsettledFor(tokenAddress)
+   *     .toPromise();
+   *
+   *   // [
+   *   //   {
+   *   //     settleTimeout: 500,
+   *   //     balance: 0,
+   *   //     partnerAddress: "0x26704469E95202191bf3Fb277669B6E6Bdd8cC65",
+   *   //     revealTimeout: 50,
+   *   //     state: "opened",
+   *   //     totalDeposit: 0,
+   *   //     tokenAddress: "0xaFF4481D10270F50f203E0763e2597776068CBc5",
+   *   //     channelIdentifier: 2,
+   *   //     tokenNetworkIdentifier: "0x26746540aBB01b15294Bf93715e4EEdAF1946110"
+   *   //   }
+   *   // ];
+   *   console.log(unsettledChannels);
+   * })()
+   * ```
    * @param tokenAddress - The address of the token with unsettled channels
    * @throws {@link RaidenAPIError}
    * @see {@link https://raiden-network.readthedocs.io/en/stable/rest_api.html#get--api-(version)-channels-(token_address)}
